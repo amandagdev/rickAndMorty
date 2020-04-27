2c2a6dc5e817c773bc4a5a73c9f0af4e9bd8cb0a
amandagdev <tiamandagomes@gmail.com>
2020-04-27 20:40:29 -0300

    scroll e paginacao

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,33 +6,40 @@ import api from '../../services/api';
 
 export default function Home() {
   const [characters, setCharacters] = useState([]);
+  const [pages, setPages] = useState(1);
+  const totalPages = 25;
 
-  const awaitCharacter = useCallback(() => {
+  async function loadPage(page = pages) {
+    console.tron.log(pages, 'pages');
+    if (totalPages && page > totalPages) return;
     try {
-      async function handleCharacter() {
-        const response = await api.get('/character');
-        setCharacters(response.data.results);
-      }
-      handleCharacter();
+      const response = await api.get(`/character/?page=${page}`);
+      const data = await response.data.results;
+      setCharacters([...characters, ...data]);
+      setPages(page + 1);
     } catch (error) {
       console.tron.log(error);
     }
-  }, []);
+  }
+
+  // const awaitCharacter = useCallback(() => {
+
+  // }, []);
 
   useEffect(() => {
-    awaitCharacter();
-  }, [awaitCharacter]);
+    loadPage();
+  }, []);
 
   return (
     <View style={styles.container}>
       {characters.length > 0 ? (
         <FlatList
           style={{width: '100%'}}
+          onEndReached={() => loadPage()}
+          onEndReachedThreshold={0.1}
           data={characters}
           keyExtractor={item => String(item.id)}
-          renderItem={({item}) => (
-            <Character character={item} />
-          )}
+          renderItem={({item}) => <Character character={item} />}
         />
       ) : (
         <View>
